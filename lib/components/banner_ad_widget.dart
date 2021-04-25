c4b81906849ad14a62be042238f66a9d9ecfcf8c
KanaanZafar <kanaanzafar7@gmail.com>
2021-04-25 14:22:35 +0500

    update facebook audience network version number

--- lib/components/banner_ad_widget.dart
+++ lib/components/banner_ad_widget.dart
@@ -1,4 +1,3 @@
-import 'package:admob_flutter/admob_flutter.dart';
 import 'package:facebook_audience_network/ad/ad_banner.dart';
 import 'package:flutter/material.dart';
 import 'package:image_comparison/utils/ad_ids.dart';
